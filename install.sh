b808c770ac468be26a6cf87d851d30dff853b06b
y0ssi10 <y0ssi10@users.noreply.github.com>
2025-06-28 12:09:45 +0000

    deploy: dd55f49609997a95f6d1e81a17c264ebeb616be3

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+INSTALL_DIR="${INSTALL_DIR:-$HOME/dotfiles}"
+
+if [ -d "$INSTALL_DIR" ]; then
+    echo "Updating dotfiles..."
+    git -C "$INSTALL_DIR" pull
+else
+    echo "Installing dotfiles..."
+    git clone https://github.com/y0ssi10/dotfiles "$INSTALL_DIR"
+fi
+
+/bin/bash "$INSTALL_DIR/scripts/setup.bash"
