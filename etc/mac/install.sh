a81ebfbb66d5c7e76529fcf06fa56a136bc70683
y0ssi10 <iwai0621@gmail.com>
2025-08-05 00:21:07 +0900

    refactor(install): streamline install.sh by removing commented-out code and updating next steps for Homebrew setup

--- etc/mac/install.sh
+++ etc/mac/install.sh
@@ -5,26 +5,8 @@ XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
-# echo "  create symlink to homebrew directory."
-# https://stackoverflow.com/questions/65259300/detect-apple-silicon-from-command-line
-# if [[ "$(uname -m)" == 'arm64' ]]; then
-#   ln -sf "/opt/homebrew" "$HOME/homebrew"
-# else
-#   ln -sf "/usr/local" "$HOME/homebrew"
-# fi
+DOT_DIR="${DOT_DIR:-${HOME}/workspace/github.com/y0ssi10/dotfiles}"
 
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
 
-# echo "  enable key-repeating for VScodeVim."
-# defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
-# defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
-# defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
-
-echo ""
-echo "  Next step: Download Xcode from AppStore."
-echo "  After download, run commands below to setup applications."
-echo ""
-echo "  sudo xcodebuild -license"
-echo "  xcode-select --install"
-echo "  (for Apple Silicon Mac) sudo softwareupdate --install-rosetta"
-echo "  bash ~/dotfiles/etc/mac/setup-brew.sh"
+bash "${DOT_DIR}/etc/mac/setup-homebrew.sh"
