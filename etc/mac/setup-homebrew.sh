100906052223c5e71be35d88969e9ecc08d7389e
y0ssi10 <iwai0621@gmail.com>
2025-08-05 00:27:43 +0900

    fix(setup-homebrew): update Brewfile path to use DOT_DIR variable for improved flexibility

--- etc/mac/setup-homebrew.sh
+++ etc/mac/setup-homebrew.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+DOT_DIR="${DOT_DIR:-${HOME}/workspace/github.com/y0ssi10/dotfiles}"
+
 die() {
   echo "$1"
   echo "  terminated."
   exit 1
 }
 
-brew_list="$(dirname "$0")/Brewfile"
+brew_list="${DOT_DIR}/etc/mac/Brewfile"
 
 which curl >> /dev/null || die "curl is required."
 which brew >> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
