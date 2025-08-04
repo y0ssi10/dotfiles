4482546a009d039dd1d7e5d8b7a9ccb6ef56b99b
y0ssi10 <iwai0621@gmail.com>
2025-08-04 23:11:24 +0900

    chore(setup-homebrew): remove commented-out symlink creation for coreutils and findutils

--- etc/mac/setup-homebrew.sh
+++ etc/mac/setup-homebrew.sh
@@ -25,10 +25,3 @@ else
   echo "  Brewfile is needed."
 fi
 brew cleanup
-
-# symlinks_dir="$DOT_DIR/etc/mac/symlinks"
-# mkdir -p "$symlinks_dir"
-# ln -sf "$(brew --prefix)/opt/coreutils/libexec/gnubin" "$symlinks_dir/core_gnubin"
-# ln -sf "$(brew --prefix)/opt/findutils/libexec/gnubin" "$symlinks_dir/find_gnubin"
-
-# ln -sf "$(brew --prefix)" "$HOME/homebrew"
