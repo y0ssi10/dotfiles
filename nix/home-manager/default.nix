6dc270519e2f366051255ba76f33570c190b63fa
y0ssi10 <iwai0621@gmail.com>
2025-08-09 13:07:57 +0900

    chore(home-manager): update stateVersion to 25.11 and add new packages; remove unused program configurations

--- nix/home-manager/default.nix
+++ nix/home-manager/default.nix
@@ -16,13 +16,60 @@
   home = {
     username = username;
     homeDirectory = "/Users/${username}";
-    stateVersion = "24.11";
+    stateVersion = "25.11";
     packages = with pkgs; [
+      bash
+      # bat
+      gettext
+      coreutils
+      curl
+      diffutils
+      # direnv
+      # docker-completion
+      # docker
+      # eza
+      # fastfetch
+      fd
+      findutils
+      # fzf
+      gawk
+      # gh
+      ghq
+      # ghr
+      git
+      # git-secrets
+      gnused
+      gnupg
+      gnugrep
+      jq
+      k9s
+      kubectl
+      # keyring
+      kind
+      # lazygit
+      # mise
+      # navi
+      # neovim
+      # nkf
+      # ripgrep
+      sheldon
+      shellcheck
+      # starship
+      tmux
+      # vim
+      zsh
+      yq
       nix-zsh-completions
-      starship
-      lazygit
-      vim
+      lazydocker
     ];
+  };
+
+  xdg = {
+    enable = true;
+    configHome = "${config.home.homeDirectory}/.config";
+    cacheHome  = "${config.home.homeDirectory}/.cache";
+    dataHome   = "${config.home.homeDirectory}/.local/share";
+    stateHome  = "${config.home.homeDirectory}/.local/state";
   };
 
   programs.home-manager.enable = true;
@@ -45,12 +92,63 @@
     ];
   };
 
-  imports = [
-    ./programs/bat.nix
-    ./programs/direnv.nix
-    ./programs/eza.nix
-  ];
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.alacritty.enable
+  # programs.alacritty.enable = true;
+  programs.bat = {
+    enable = true;
+    extraPackages = with pkgs.bat-extras; [
+      batdiff
+      batgrep
+      batman
+      batpipe
+      batwatch
+      prettybat
+    ];
+  };
+
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.bat.enable
+  programs.direnv = {
+    enable = true;
+    # config = {
+    #   disable_stdin = true;
+    #   strict_env = true;
+    #   warn_timeout = 0;
+    # };
+    # nix-direnv.enable = true;
+  };
+
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.eza.enable
+  programs.eza = {
+    enable = true;
+    colors = "auto";
+    git = true;
+    icons = "auto";
+    extraOptions = [
+      "--group-directories-first"
+    ];
+  };
 
   # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.fastfetch.enable
   programs.fastfetch.enable = true;
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.fd.enable
+  programs.fd.enable = true;
+  programs.fzf.enable = true;
+  programs.ghostty.enable = true;
+  programs.lazygit.enable = true;
+  programs.mise.enable = true;
+  programs.navi.enable = true;
+  programs.neovim.enable = true;
+  programs.ripgrep = {
+    enable = true;
+    arguments = [
+      "--hidden"
+      "--smart-case"
+
+      "--glob=!.git/"
+    ];
+  };
+  programs.starship.enable = true;
+  # programs.tmux.enable = true;
+  programs.vim.enable = true;
+  # programs.zsh.enable = true;
 }
--- nix/nix-darwin/default.nix
+++ nix/nix-darwin/default.nix
@@ -12,6 +12,7 @@
     };
     settings = {
       experimental-features = "nix-command flakes";
+      use-xdg-base-directories = true;
       max-jobs = 8;
     };
   };
@@ -32,7 +33,7 @@
 
   system = {
     stateVersion = 6;
-    primaryUser = "y0ssi10";
+    primaryUser = "runner";
     defaults = {
       NSGlobalDomain = {
         AppleShowAllExtensions = true;
@@ -79,99 +80,9 @@
       autoUpdate = true;
     };
 
-    taps = [
-      "homebrew/bundle"
-    ];
-
     brews = [
-      # Bourne-Again SHell, a UNIX command interpreter
-      "bash"
-      # Cryptography and SSL/TLS Toolkit
-      "openssl@3"
-      # Clone of cat(1) with syntax highlighting and Git integration
-      "bat"
-      # GNU internationalization (i18n) and localization (l10n) library
-      "gettext"
-      # GNU File, Shell, and Text utilities
-      "coreutils"
-      # Get a file from an HTTP, HTTPS or FTP server
-      "curl"
-      # File comparison utilities
-      "diffutils"
-      # Load/unload environment variables based on $PWD
-      "direnv"
-      # Bash, Zsh and Fish completion for Docker
-      "docker-completion"
-      # Pack, ship and run any application as a lightweight container
-      "docker"
-      # Modern, maintained replacement for ls
-      "eza"
-      # Like neofetch, but much faster because written mostly in C
-      "fastfetch"
-      # Simple, fast and user-friendly alternative to find
-      "fd"
-      # Collection of GNU find, xargs, and locate
-      "findutils"
-      # Command-line fuzzy finder written in Go
-      "fzf"
-      # GNU awk utility
-      "gawk"
-      # GitHub command-line tool
-      "gh"
-      # Remote repository management made easy
-      "ghq"
-      # Upload multiple artifacts to GitHub Release in parallel
-      "ghr"
-      # Distributed revision control system
-      "git"
-      # Prevents you from committing sensitive information to a git repo
-      "git-secrets"
-      # GNU implementation of the famous stream editor
-      "gnu-sed"
-      # GNU Transport Layer Security (TLS) Library
-      "gnutls"
-      # GNU Pretty Good Privacy (PGP) package
-      "gnupg"
-      # GNU grep, egrep and fgrep
-      "grep"
-      # Lightweight and flexible command-line JSON processor
-      "jq"
-      # Kubernetes CLI To Manage Your Clusters In Style!
-      "k9s"
-      # Easy way to access the system keyring service from python
-      "keyring"
-      # Run local Kubernetes cluster in Docker
-      "kind"
-      # Simple terminal UI for git commands
-      "lazygit"
-      # Polyglot runtime manager (asdf rust clone)
-      "mise"
-      # Interactive cheatsheet tool for the command-line
-      "navi"
-      # Ambitious Vim-fork focused on extensibility and agility
-      "neovim"
-      # Network Kanji code conversion Filter (NKF)
-      "nkf"
       # Pinentry for GPG on Mac
       "pinentry-mac"
-      # Search tool like grep and The Silver Searcher
-      "ripgrep"
-      # Fast, configurable, shell plugin manager
-      "sheldon"
-      # Static analysis and lint tool, for (ba)sh scripts
-      "shellcheck"
-      # Cross-shell prompt for astronauts
-      "starship"
-      # Terminal multiplexer
-      "tmux"
-      # Vi 'workalike' with many additional features
-      "vim"
-      # Tool for creating isolated virtual python environments
-      "virtualenv"
-      # Process YAML, JSON, XML, CSV and properties documents from the CLI
-      "yq"
-      # UNIX shell (command interpreter)
-      "zsh"
     ];
 
     casks = [
@@ -191,7 +102,7 @@
       # Japanese input software
       "google-japanese-ime"
       # Keymap tool for Happy Hacking Keyboard Professional (Hybrid models only)
-      "hhkb-keymap-tool"
+      "hhkb"
       # Software for Logitech devices
       "logi-options+"
       # Replacement for Docker Desktop
