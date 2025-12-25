60e28ca4e0169da044fb19f31daf1ec174835448
y0ssi10 <iwai0621@gmail.com>
2025-12-25 23:11:18 +0900

    refactor(flake): streamline Nix configuration by updating input sources and modularizing app definitions

--- flake.nix
+++ flake.nix
@@ -1,31 +1,14 @@
 {
-  description = "y0ssi10's dotfiles";
+  description = "y0ssi10's Nix configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-compat.follows = "flake-compat";
-    };
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-    };
-    hercules-ci-effects = {
-      url = "github:hercules-ci/hercules-ci-effects";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-parts.follows = "flake-parts";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin";
+      url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -37,95 +20,32 @@
       home-manager,
       nix-darwin,
       ...
-    }@inputs:
+    }:
     let
       system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
     in
     {
-      apps.${system} = {
-        update = {
-          type = "app";
-          program = toString (
-            pkgs.writeShellScript "update-script" ''
-              set -e
+      apps = (
+        import ./nix/apps {
+          inherit nixpkgs;
+        }
+      );
 
-              echo "Updating flake..."
-              nix flake update
-              echo "Updating home-manager..."
-              nix run nixpkgs#home-manager -- switch --flake .#y0ssi10-home
-              echo "Updating nix-darwin..."
-              nix run nix-darwin -- switch --flake .#y0ssi10-darwin
-              echo "Update complete!"
-            ''
-          );
-        };
+      darwinConfigurations = (
+        import ./nix/nix-darwin {
+          inherit system nix-darwin;
+        }
+      );
 
-        update-home = {
-          type = "app";
-          program = toString (
-            pkgs.writeShellScript "update-script" ''
-              set -e
-
-              echo "Updating flake..."
-              nix flake update
-              echo "Updating home-manager..."
-              nix run nixpkgs#home-manager -- switch --flake .#y0ssi10-home
-            ''
-          );
-        };
-        update-darwin = {
-          type = "app";
-          program = toString (
-            pkgs.writeShellScript "update-script" ''
-              set -e
-
-              echo "Updating flake..."
-              nix flake update
-              echo "Updating nix-darwin..."
-              nix run nix-darwin -- switch --flake .#y0ssi10-darwin
-              echo "Update complete!"
-            ''
-          );
-        };
-      };
-
-      darwinConfigurations = {
-        y0ssi10-darwin = nix-darwin.lib.darwinSystem {
-          system = system;
-          specialArgs = {
-            username = "y0ssi10";
+      homeConfigurations = (
+        import ./nix/home-manager {
+          inherit system home-manager;
+          username = "y0ssi10";
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
           };
-          modules = [ ./nix/nix-darwin/default.nix ];
-        };
-
-        runner-darwin = nix-darwin.lib.darwinSystem {
-          system = system;
-          specialArgs = {
-            username = "runner";
-          };
-          modules = [ ./nix/nix-darwin/default.nix ];
-        };
-
-      };
-
-      homeConfigurations = {
-        y0ssi10-home = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-          extraSpecialArgs = {
-            username = "y0ssi10";
-            inherit inputs;
-          };
-          modules = [ ./nix/home-manager/default.nix ];
-        };
-        runner-home = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-          extraSpecialArgs = {
-            username = "runner";
-            inherit inputs;
-          };
-          modules = [ ./nix/home-manager/default.nix ];
-        };
-      };
+        }
+      );
     };
 }
