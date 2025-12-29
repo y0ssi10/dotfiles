fbc6de3b06d697e6d59c1fe3a811e1c0a5f6bb1d
y0ssi10 <iwai0621@gmail.com>
2025-12-29 23:40:26 +0900

    feat(install): add installation functions for Xcode, Homebrew, and Nix with bootstrap support

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e # エラー発生時に終了
 
-# XDG環境変数のデフォルト値を設定
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
@@ -25,11 +24,17 @@ DIALOG="
   (2) link dotfiles
   (3) setup applications
 
+  This script also includes for Nix:
+  (1') install Xcode, Homebrew and Nix
+  (2') bootstrap Nix
+
   Select:
-  [a] run all above
+  [a] run (1) (2) (3) above steps
   [d] only download dotfiles
   [l] only link dotfiles
   [s] only setup applications
+  [i] only install Xcode, Homebrew and Nix
+  [b] only bootstrap Nix
   You can use multiple choose like 'dl'
 "
 
@@ -53,6 +58,83 @@ OS="unknown"
 if [ "$(uname)" = "Darwin" ]; then
   OS="mac"
 fi
+
+install_xcode() {
+  echo "Try to install Xcode Command Line Tools..."
+
+  if xcode-select -p > /dev/null 2>&1; then
+    echo "Xcode Command Line Tools is already installed."
+  else
+    sudo xcodebuild -license
+    xcode-select --install
+    echo "Xcode Command Line Tools installed successfully."
+  fi
+
+  if [ "$(uname -m)" = "arm64" ]; then
+    if ! /usr/bin/pgrep -q oahd; then
+      echo "Installing Rosetta..."
+      sudo softwareupdate --install-rosetta  --agree-to-license
+      echo "Rosetta installed successfully."
+    else
+      echo "Rosetta is already installed."
+    fi
+  fi
+}
+
+install_nix() {
+  if has "nix"; then
+    echo "Nix is already installed."
+  else
+    echo "Running (determinate) nix installer..."
+    # use Determinate Nix installer, https://github.com/DeterminateSystems/nix-installer
+    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm	--determinate
+    echo "Nix installed successfully."
+  fi
+
+  set +o errexit
+  set +o nounset
+  set +o pipefail
+  # load nix commands into the current shell, so that we don't have to open a new one
+  # shellcheck disable=SC1091
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  set -o errexit
+  set -o nounset
+  set -o pipefail
+
+  nix --version
+  echo "configured Nix environment successfully."
+}
+
+install_homebrew() {
+  has "curl" || die "curl is required."
+  has "brew" || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  has "brew" || die "brew is required."
+
+  eval "$(/opt/homebrew/bin/bew shellenv)"
+  brew doctor || die "brew doctor raised error."
+
+  echo "  Updating Homebrew..."
+  brew update
+}
+
+bootstrap_nix() {
+  echo "Bootstrapping nix with 'default' configuration..."
+  {
+    echo "Backing up /etc/* files..."
+    [ ! -f /etc/zshenv ] || sudo mv /etc/zshenv /etc/zshenv.before-nix-darwin
+    [ ! -f /etc/zshrc ] || sudo mv /etc/zshrc /etc/zshrc.before-nix-darwin
+    [ ! -f /etc/zprofile ] || sudo mv /etc/zprofile /etc/zprofile.before-nix-darwin
+
+    # To use flake.nix and default configuration stored in dotfiles repo:
+    echo "Bootstrapping nix with 'default' configuration successfully."
+  } || {
+    echo "Bootstrapping nix with 'default' configuration failed, restoring /etc/* files..."
+    [ -f /etc/zshenv.before-nix-darwin ] && sudo mv /etc/zshenv.before-nix-darwin /etc/zshenv
+    [ -f /etc/zshrc.before-nix-darwin ] && sudo mv /etc/zshrc.before-nix-darwin /etc/zshrc
+    [ -f /etc/zprofile.before-nix-darwin ] && sudo mv /etc/zprofile.before-nix-darwin /etc/zprofile
+    echo "Bootstrapping nix with 'default' configuration failed, restoring /etc/* files successfully."
+  }
+}
 
 download_dotfiles() {
   if [ -d "$DOT_DIR" ]; then
@@ -125,6 +207,28 @@ if [[ "$selection" = *"a"* ]] || [[ "$selection" = *"s"* ]]; then
   os_install_sh="${DOT_DIR}/etc/${OS}/install.sh"
   [ -f "$os_install_sh" ] && sh -c "$os_install_sh"
   echo "  end setup applications."
+  echo ""
+fi
+
+if [[ "$selection" = *"i"* ]]; then
+  echo "  begin install XCode Command Line Tools..."
+  install_xcode
+  echo "  end install XCode Command Line Tools."
+  echo ""
+  echo "  begin install Homebrew..."
+  install_homebrew
+  echo "  end install Homebrew."
+  echo ""
+  echo "  begin install Nix..."
+  install_nix
+  echo "  end install Nix."
+  echo ""
+fi
+
+if [[ "$selection" = *"b"* ]]; then
+  echo "  begin bootstrap Nix..."
+  bootstrap_nix
+  echo "  end bootstrap Nix."
   echo ""
 fi
 
